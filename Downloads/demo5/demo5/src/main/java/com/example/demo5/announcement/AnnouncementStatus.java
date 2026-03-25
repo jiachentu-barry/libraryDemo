abe7f259d360a87bdf52a5324ba7c2060a2cf847
JiaChen Tu <83070640+jiachentu-barry@users.noreply.github.com>
2026-03-25 17:54:26 +0900

    Delete Downloads/demo5/demo5 directory

--- Downloads/demo5/demo5/src/main/java/com/example/demo5/announcement/AnnouncementStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo5.announcement;
-
-public enum AnnouncementStatus {
-    DRAFT,
-    PUBLISHED
-}
--- Downloads/demo5/demo5/src/main/java/com/example/demo5/book/BookStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo5.book;
-
-public enum BookStatus {
-    NORMAL,
-    RECOMMENDED
-}
